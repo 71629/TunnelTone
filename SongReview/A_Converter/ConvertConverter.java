3a5c57af2c4ec21fcc775fe3d340eacb44f4e5e3
kinglingmk1 <jackymak2004@gmail.com>
2023-10-16 20:40:43 +0800

    Delete some useless code

--- SongReview/A_Converter/ConvertConverter.java
+++ SongReview/A_Converter/ConvertConverter.java
@@ -8,30 +8,19 @@ class ConvertConverter {
 		File file = new File(
 		 	"..\\A_Converter\\2.aff");
 		BufferedReader br = new BufferedReader(new FileReader(file));
-		
-		int loop = 87; int checkloop = 0; int count = 0; int counttt = 0; int doublecount = 0; int saveI = 0;
-
-		float rand = 0; float looptemp = 0;
-
-		String save = ""; String tempsave = ""; String st; String temp; String storetemp = "s";
+		int count = 0; int counttt = 0;
 		String arcSave[] = new String [9999];
 		String arcTapSave[] = new String [9999];
 		String narcSave[] = new String [9999];
 		String carcSave[] = new String [9999];
-		int saveNote[] = new int [9999];
-		boolean trigger = true;
-			
-		System.out.print("Remember change the location in line 9, 70 and 74 of this program to read and write the file!!!");
-		System.out.println();
+		//System.out.print("Remember change the location in line 9, 70 and 74 of this program to read and write the file!!!");
+		//System.out.println();
 		//Read per line until end
 		while ((st = br.readLine()) != null) {
 			if(counttt >=3) {
 				arcSave[count] = st;
 				count++;
-			} else {
-				counttt++;
-			}
-			
+			} else {counttt++;}
 		}
 		for(int i=0; i<arcSave.length; i++) {
 			if(arcSave[i]!=null) {
