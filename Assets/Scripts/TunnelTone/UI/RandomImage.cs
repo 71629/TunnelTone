15970d33283b2c4cbac881f65ac3456eea518120
moonlet023 <moonlet023@gmail.com>
2023-10-24 10:29:58 +0800

    update
    photo move Demo 1

--- Assets/Scripts/TunnelTone/UI/RandomImage.cs
+++ Assets/Scripts/TunnelTone/UI/RandomImage.cs
@@ -7,28 +7,34 @@ using UnityEngine.UI;
 
 public class RandomImage : MonoBehaviour
 {
+    public Image image;
     public Sprite sprite;
     public Sprite[] Sprites;
     public SpriteRenderer spriteRenderer;
+    public float time;
+    public float timerate = 10;
    
     
 
     void Start()
     {
-        
+        image = GetComponent<Image>();
     }
 
     private void Update()
     {
-        changeSprite();
+        time += Time.deltaTime;
+        if (time >= timerate)
+        {
+            changeSprite();
+            time = 0;
+        }
     }
 
     void changeSprite()
     {
-
-        int num = UnityEngine.Random.Range(0, Sprites.Length);
-        sprite = Sprites[num];
-        spriteRenderer.sprite = sprite;
-
+            int num = UnityEngine.Random.Range(0, Sprites.Length);
+                    sprite = Sprites[num];
+                    image.sprite = sprite;
     }
 }
