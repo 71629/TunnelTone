278ff0f03eb024495f1e118bef80453282ff4a73
moonlet023 <moonlet023@gmail.com>
2023-11-13 11:08:26 +0800

    fadein fadeout with 2 image

--- Assets/Scripts/TunnelTone/UI/RandomImage.cs
+++ Assets/Scripts/TunnelTone/UI/RandomImage.cs
@@ -7,18 +7,20 @@ using UnityEngine.UI;
 
 public class RandomImage : MonoBehaviour
 {
-    public Image image, image2;
+    public Image image;
     public Sprite sprite;
     public Sprite[] Sprites;
     public SpriteRenderer spriteRenderer;
     public float time;
     public float timerate = 10;
+    public int startTimeRate = 0;
    
     
 
-    void Start()
+    IEnumerator Start()
     {
         image = GetComponent<Image>();
+        yield return new WaitForSecondsRealtime(startTimeRate);
         StartCoroutine(FadeInFadeOut());
     }
 
@@ -37,49 +39,31 @@ public class RandomImage : MonoBehaviour
             int num = UnityEngine.Random.Range(0, Sprites.Length);
             sprite = Sprites[num];
             image.sprite = sprite;
-            image2.sprite = sprite;
     }
 
     IEnumerator FadeInFadeOut()
     {
+        
         while (true)
         {
             yield return new WaitForSecondsRealtime(timerate);
-
-            int loop = 0;
-
-            if (loop == 0)
-            {
                 for (float i = 1; i >= 0; i -= Time.deltaTime)
                 {
                     yield return image.color = new Color((float)0.2196079, (float)0.2196079, (float)0.2196079, i);
                     Debug.Log("image.a = " + image.color.a);
                 }
+
+                yield return new WaitForSecondsRealtime(3);
                 changeSprite();
-                for (float i = 0; i <= 1; i += Time.deltaTime)
-                {
-                    yield return image2.color = new Color((float)0.2196079, (float)0.2196079, (float)0.2196079, i);
-                    Debug.Log("image.a = " + image.color.a);
-                }
-                changeSprite();
-                loop += 1;
-            }
-            else
-            {
-                for (float i = 1; i >= 0; i -= Time.deltaTime)
-                {
-                    yield return image2.color = new Color((float)0.2196079, (float)0.2196079, (float)0.2196079, i);
-                    Debug.Log("image.a = " + image.color.a);
-                }
-                changeSprite();
+                yield return new WaitForSecondsRealtime(3);
+                
                 for (float i = 0; i <= 1; i += Time.deltaTime)
                 {
                     yield return image.color = new Color((float)0.2196079, (float)0.2196079, (float)0.2196079, i);
                     Debug.Log("image.a = " + image.color.a);
                 }
-                changeSprite();
-                loop -= 1;
-            }
+            
         }
     }
     }
+    
