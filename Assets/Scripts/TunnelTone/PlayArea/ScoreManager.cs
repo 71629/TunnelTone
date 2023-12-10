fbb4fd6e1e66b3374bf3b2571f9115e61cb12c10
71629 <105049686+71629@users.noreply.github.com>
2023-12-10 22:10:57 +0800

    EUREKA!! Score calculation issue is resolved

--- Assets/Scripts/TunnelTone/PlayArea/ScoreManager.cs
+++ Assets/Scripts/TunnelTone/PlayArea/ScoreManager.cs
@@ -10,10 +10,12 @@ namespace TunnelTone.PlayArea
     public class ScoreManager : Singleton<ScoreManager>
     {
         public int totalCombo;
-        private const float MaxScore = 10000000;
+        private const decimal MaxScore = 10000000;
         private TextMeshProUGUI Score => GetComponent<TextMeshProUGUI>();
-        private static float CurrentScore { get; set; }
-        private static float DisplayScore { get; set; }
+        private static decimal CurrentScore { get; set; }
+        private static decimal DisplayScore { get; set; }
+        
+        private const int LerpIteration = 30;
 
         private void Start()
         {
@@ -31,16 +33,16 @@ namespace TunnelTone.PlayArea
             switch (offset)
             {
                 case <= 25:
-                    CurrentScore += MaxScore + 10000f / totalCombo;
-                    StartCoroutine(UpdateDisplay((MaxScore + 10000) / totalCombo / 30f, 30));
+                    CurrentScore += MaxScore + 10000 / (decimal)totalCombo;
+                    StartCoroutine(UpdateDisplay((MaxScore + 10000) / totalCombo / LerpIteration, LerpIteration));
                     break;
                 case <= 50:
                     CurrentScore += MaxScore / totalCombo;
-                    StartCoroutine(UpdateDisplay(MaxScore / totalCombo / 30f, 30));
+                    StartCoroutine(UpdateDisplay(MaxScore / totalCombo / LerpIteration, LerpIteration));
                     break;
                 case <= 100:
-                    CurrentScore += 0.6f * (MaxScore / totalCombo);
-                    StartCoroutine(UpdateDisplay(0.6f * (MaxScore / totalCombo / 30f), 30));
+                    CurrentScore += 0.6m * (MaxScore / totalCombo);
+                    StartCoroutine(UpdateDisplay(0.6m * (MaxScore / totalCombo / LerpIteration), LerpIteration));
                     break;
                 default:
                     CurrentScore += 0;
@@ -48,10 +50,9 @@ namespace TunnelTone.PlayArea
             }
         }
 
-        private IEnumerator UpdateDisplay(float delta, int remain)
+        private IEnumerator UpdateDisplay(decimal delta, int remain)
         {
             yield return null;
-            
             if (remain == 0) yield break;
 
             DisplayScore += delta;
