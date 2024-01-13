0c2bbf1cf53ce9de23a87c3c8df12077923b9f04
71629 <105049686+71629@users.noreply.github.com>
2024-01-13 22:24:37 +0800

    Before merge

---
+++ Assets/Scripts/TunnelTone/PlayArea/PlayAreaDecoration.cs
@@ -0,0 +1,25 @@
+﻿using TunnelTone.Events;
+using TunnelTone.ScriptableObjects;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace TunnelTone.PlayArea
+{
+    public class PlayAreaDecoration : MonoBehaviour
+    {
+        [SerializeField] private Image jacket;
+
+        private void Start()
+        {
+            SystemEvent.OnChartLoad.AddListener(o =>
+            {
+                var songData = (SongData)o[0];
+                
+                if (songData.jacket is not null)
+                {
+                    jacket.sprite = songData.jacket;
+                }
+            });
+        }
+    }
+}
