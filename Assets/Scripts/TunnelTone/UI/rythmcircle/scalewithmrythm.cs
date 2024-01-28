04fc8a374d500346b6875bd7d222c5c3e9d8f9ff
moonlet023 <moonlet023@gmail.com>
2024-01-28 21:50:02 +0800

    line draw(fail)

--- Assets/Scripts/TunnelTone/UI/rythmcircle/scalewithmrythm.cs
+++ Assets/Scripts/TunnelTone/UI/rythmcircle/scalewithmrythm.cs
@@ -9,17 +9,19 @@ namespace TunnelTone.UI.rythmcircle
     public class scalewithmrythm : MonoBehaviour
     {
         GameObject _circle;
-        //public AudioSource _source;
         public AudioSource audioClip;
         public float[] _testbar;
-        //public float scalesize;
+        public LineRenderer line;
+        private int count = 32;
         
         // Start is called before the first frame update
         void Start()
         {
             _testbar = new float[64];
             _circle = this.gameObject;
-            //_source = this.GetComponent<AudioSource>();
+            line.positionCount = count;
+            line.startWidth = 0.02f;
+            line.endWidth = 0.02f;
         }
     
         // Update is called once per frame
@@ -28,6 +30,11 @@ namespace TunnelTone.UI.rythmcircle
             //_source = AudioManager.Instance.audioSource;
             //_source.clip = audioClip.clip;
             GetAudioSpectrumData();
+            for (int i = 0; i < count; i++)
+            {
+                var v = _testbar[i];
+                line.SetPosition(i, new Vector3((i - (count / 2) * 0.2f), v * 0.2f, -5));
+            }
         }
 
         void GetAudioSpectrumData()
