0f2e0a68b73ff32fc3ccf80e5ff3283a4e0a5570
71629 <105049686+71629@users.noreply.github.com>
2024-01-29 14:53:23 +0800

    Stable gradient

--- Assets/Scripts/TunnelTone/Elements/SongBarColor.cs
+++ Assets/Scripts/TunnelTone/Elements/SongBarColor.cs
@@ -1,84 +1,86 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-public class SongBarColor : MonoBehaviour
+
+namespace TunnelTone.Elements
 {
-    [SerializeField] Button pause, retry, resume, quit, start;
-    [SerializeField] bool isPlaying; 
+    public class SongBarColor : MonoBehaviour
+    {
+        [SerializeField] Button pause, retry, resume, quit, start;
+        [SerializeField] bool isPlaying; 
 
-    private Image SongBar, Difficulty, LengthsBar;
-    private GameObject GSongBar;
+        private Image SongBar, Difficulty, LengthsBar;
+        private GameObject GSongBar;
 
-    private AudioSource audioSource;
-    private float dspTimeEnd, currentTime;
+        private AudioSource audioSource;
+        private float dspTimeEnd, currentTime;
 
-    private Vector2 fillVector2;
+        private Vector2 fillVector2;
 
-    private bool isPlayingSetOnce;
+        private bool isPlayingSetOnce;
     
-    // Update is called once per frame
-    void Start()
-    {
-        GSongBar = GameObject.Find("Music Play/UI/Progress/Fill Area/Fill");
-        fillVector2 = GSongBar.transform.position;
-
-        SongBar = GameObject.Find("Music Play/UI/Progress/Fill Area/Fill").GetComponent<Image>();
-        Difficulty = GameObject.Find("Music Play/UI/Difficulty").GetComponent<Image>();
-        LengthsBar = GameObject.Find("Music Play/UI/Progress/Fill Area/Fill/Image").GetComponent<Image>();
-
-        audioSource = GameObject.Find("Basic/Main Camera/Music Player").GetComponent<AudioSource>();
-
-        start.onClick.AddListener(() => startOnClick());
-        pause.onClick.AddListener(() => LeanTween.pause(GSongBar));
-        resume.onClick.AddListener(() => LeanTween.pause(GSongBar));
-        retry.onClick.AddListener(() => retryOnClick());
-        quit.onClick.AddListener(() => quitOnClick());
-
-        LengthsBar.rectTransform.sizeDelta = new Vector2(Screen.width, LengthsBar.rectTransform.sizeDelta.y);
-        LengthsBar.transform.position = new Vector2((-86.5f * 2), LengthsBar.transform.position.y);
-        isPlaying = false;
-        isPlayingSetOnce = false;
-        dspTimeEnd = 0f;
-    }
-    void Update()
-    {
-        if(isPlaying)
+        // Update is called once per frame
+        void Start()
         {
-            currentTime = audioSource.time;
-            dspTimeEnd = audioSource.clip.length;
-            if (SongBar.color != Difficulty.color)
+            GSongBar = GameObject.Find("Music Play/UI/Progress/Fill Area/Fill");
+            fillVector2 = GSongBar.transform.position;
+
+            SongBar = GameObject.Find("Music Play/UI/Progress/Fill Area/Fill").GetComponent<Image>();
+            Difficulty = GameObject.Find("Music Play/UI/Difficulty").GetComponent<Image>();
+            LengthsBar = GameObject.Find("Music Play/UI/Progress/Fill Area/Fill/Image").GetComponent<Image>();
+
+            audioSource = GameObject.Find("Basic/Main Camera/Music Player").GetComponent<AudioSource>();
+
+            start.onClick.AddListener(() => startOnClick());
+            pause.onClick.AddListener(() => LeanTween.pause(GSongBar));
+            resume.onClick.AddListener(() => LeanTween.pause(GSongBar));
+            retry.onClick.AddListener(() => retryOnClick());
+            quit.onClick.AddListener(() => quitOnClick());
+
+            LengthsBar.rectTransform.sizeDelta = new Vector2(Screen.width, LengthsBar.rectTransform.sizeDelta.y);
+            LengthsBar.transform.position = new Vector2((-86.5f * 2), LengthsBar.transform.position.y);
+            isPlaying = false;
+            isPlayingSetOnce = false;
+            dspTimeEnd = 0f;
+        }
+        void Update()
+        {
+            if(isPlaying)
             {
-                SongBar.color = Color.white;
-                LengthsBar.color = Difficulty.color;
+                currentTime = audioSource.time;
+                dspTimeEnd = audioSource.clip.length;
+                if (SongBar.color != Difficulty.color)
+                {
+                    SongBar.color = Color.white;
+                    LengthsBar.color = Difficulty.color;
+                }
+            }
+            if (isPlayingSetOnce&& (Vector2)GSongBar.transform.position == fillVector2)
+            {
+                LeanTween.moveX(GSongBar, 85.5f, dspTimeEnd);
+                isPlayingSetOnce = false;
             }
         }
-        if (isPlayingSetOnce&& (Vector2)GSongBar.transform.position == fillVector2)
+        private void retryOnClick()
         {
-            LeanTween.moveX(GSongBar, 85.5f, dspTimeEnd);
-            isPlayingSetOnce = false;
+            LeanTween.cancel(GSongBar);
+            GSongBar.transform.position = new Vector2(-86.15f, 47.60f);
+            LeanTween.move(GSongBar, fillVector2, 0.1f);
+            GSongBar.transform.position = new Vector2(0, GSongBar.transform.position.z);
+            isPlayingSetOnce = true;
         }
-    }
-    private void retryOnClick()
-    {
-        LeanTween.cancel(GSongBar);
-        GSongBar.transform.position = new Vector2(-86.15f, 47.60f);
-        LeanTween.move(GSongBar, fillVector2, 0.1f);
-        GSongBar.transform.position = new Vector2(0, GSongBar.transform.position.z);
-        isPlayingSetOnce = true;
-    }
-    private void quitOnClick()
-    {
-        isPlaying = false;
-        LeanTween.cancel(GSongBar);
-        GSongBar.transform.position = new Vector2(-86.15f, 47.60f);
-        LeanTween.move(GSongBar, fillVector2, 0.1f);
-        GSongBar.transform.position = new Vector2(0, GSongBar.transform.position.z);
-    }
-    private void startOnClick()
-    {
-        isPlayingSetOnce = true;
-        isPlaying = true;
-        LeanTween.move(GSongBar, fillVector2, 0.1f);
+        private void quitOnClick()
+        {
+            isPlaying = false;
+            LeanTween.cancel(GSongBar);
+            GSongBar.transform.position = new Vector2(-86.15f, 47.60f);
+            LeanTween.move(GSongBar, fillVector2, 0.1f);
+            GSongBar.transform.position = new Vector2(0, GSongBar.transform.position.z);
+        }
+        private void startOnClick()
+        {
+            isPlayingSetOnce = true;
+            isPlaying = true;
+            LeanTween.move(GSongBar, fillVector2, 0.1f);
+        }
     }
 }
--- Assets/Scripts/TunnelTone/PlayArea/ProgressBarManager.cs
+++ Assets/Scripts/TunnelTone/PlayArea/ProgressBarManager.cs
@@ -1,5 +1,9 @@
-﻿using TunnelTone.Enumerators;
+﻿using TunnelTone.Elements;
+using TunnelTone.Enumerators;
+using TunnelTone.Events;
 using TunnelTone.GameSystem;
+using TunnelTone.ScriptableObjects;
+using TunnelTone.UI.Reference;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -11,9 +15,29 @@ namespace TunnelTone.PlayArea
         private int TotalSample => audioSource.clip.samples;
         private Slider Slider => GetComponent<Slider>();
 
+        private void Start()
+        {
+            SystemEvent.OnChartLoad.AddListener(SetProgressBarColor);
+        }
+
+        private void SetProgressBarColor(object[] param)
+        {
+            var songData = (SongData)param[0];
+            var difficulty = (int)param[1];
+            
+            Slider.fillRect.GetComponent<Image>().color = difficulty switch
+            {
+                0 => UIElementReference.Instance.easy,
+                1 => UIElementReference.Instance.hard,
+                2 => UIElementReference.Instance.intensive,
+                3 => UIElementReference.Instance.insane,
+                _ => Color.magenta
+            };
+        }
+
         private void Update()
         {
-            if(GameStatusReference.Instance.GameStatus is GameStatus.MusicPlay)
+            if(NoteRenderer.isPlaying)
                 Slider.value = audioSource.timeSamples / (float)TotalSample;
         }
     }
