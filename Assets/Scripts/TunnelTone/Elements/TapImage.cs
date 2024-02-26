508697b3869b27579695c5b24147fe525dbe3791
kinglingmk1 <jackymak2004@gmail.com>
2024-02-26 16:18:58 +0800

    Tap scale and texture update
     d

--- Assets/Scripts/TunnelTone/Elements/TapImage.cs
+++ Assets/Scripts/TunnelTone/Elements/TapImage.cs
@@ -6,21 +6,15 @@ using UnityEngine.UI;
 public class TapImage : MonoBehaviour
 {
     [SerializeField] private Image tapImage;
-    [SerializeField] private Transform changeImange;
+    [SerializeField] private GameObject tapPref;
 
     private void Start()
     {
-        tapImage = GetComponent<Image>();
+        //tapImage = GetComponent<Image>();
     }
 
     private void Update()
     {
-        var images = changeImange.GetComponentsInChildren<Image>();
-
-        foreach (var image in images)
-        {
-            image.sprite = tapImage.sprite;
-            image.color = tapImage.color;
-        }
+        tapPref.GetComponent<Image>().sprite = tapImage.sprite;
     }
 }
